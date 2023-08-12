1668778bbd4d523df609a69c05978728a7f437bb
mehtanishad <mehtanishad317@gmail.com>
2023-08-12 12:25:40 +0530

    daily task

--- string 2.py
+++ string 2.py
@@ -1,4 +1,4 @@
-def count_chars(str):
+def count(str):
      upper_ctr, lower_ctr, number_ctr, special_ctr = 0, 0, 0, 0
      for i in range(len(str)):
           if str[i] >= 'A' and str[i] <= 'Z': upper_ctr += 1
@@ -7,9 +7,8 @@ def count_chars(str):
           else : special_ctr += 1
      return upper_ctr, lower_ctr, number_ctr, special_ctr 
            
-str = "@Hello Ahmedaba d1 23"
-print("Original Substrings:",str)
-u, l, n, s = count_chars(str)
+str = "@Hello Ahmedabad123"
+u, l, n, s = count(str)
 print('\nUpper case characters: ',u)
 print('Lower case characters: ',l)
 print('Number case: ',n)
