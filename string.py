86c9d994d123ec65eb3979e8278a88b7e3de41bd
mehtanishad <mehtanishad317@gmail.com>
2023-08-12 12:15:29 +0530

    daily task

---
+++ string.py
@@ -0,0 +1,24 @@
+def count_list_items(strings_list):
+    count = len(strings_list)
+    return count
+
+
+string1 = ["1", "2", "3", "4", "5"]
+string2 = [" ", " "]
+string3 = ["jay", ]
+string4 = ["NISHAD"]
+
+total_count = count_list_items(string1)
+total_count2 = count_list_items(string2)
+total_count3 = count_list_items(string3)
+total_count4 = count_list_items(string4)
+
+print("Total count of number:", total_count)
+print("Total count of space:", total_count2)
+print("Total count of lowercase:", total_count3)
+print("Total count of Uppercase:", total_count4)
+
+
+
+
+    
