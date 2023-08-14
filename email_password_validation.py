2c9de101a811c37c4d5c151993420cedb4358bba
mehtanishad <mehtanishad317@gmail.com>
2023-08-14 15:53:43 +0530

    daily task

--- email_password_validation.py
+++ email_password_validation.py
@@ -2,20 +2,20 @@ import re
 user = {}
 
 def validate_password(password):
-    if 4 <= len(password) <= 8 and re.match(r'([A-Za-z])([A-Za-z\d@$!%*?&])[1234567890]', password):
+    if 4 <= len(password) <=8 and re.search(r'([A-Za-z])([A-Za-z\d@$!%*?&])[1234567890]', password):
         return True
-    return False 
+    return False
 
 
 email = input("enter your email: ")
 password = input("enter your password: ")
 
-if validate_password(password):
+if validate_password(password)==True:
     user["email"] = email
     user["password"] = password
     print("user information:", user)
     
 else:
-    print("password length 4 to 8, must contain at least one characters,one number and one special character : ")
+    print("paaword does not requirements : ")
 
 
